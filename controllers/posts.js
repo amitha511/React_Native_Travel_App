0579326242cf420efb234ab3386b8b00c9d6bbde
Idankay <idanm399@cs.colman.ac.il>
2023-04-20 14:51:54 +0300

    posts update

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,27 +2,32 @@ const Post = require('../models/post_models')
 
 const getPosts = async (req, res, next) => {
     try {
-        const posts = await Post.find();
+        let posts = await Post.find()
+        if (req.query.sender == null) {
+            posts = await Post.find()
+        } else {
+            posts = await Post.find({ 'sender': req.query.sender })
+        }
         res.status(200).send({
             'status': 'ok',
             'posts': posts
         });
     } catch (error) {
-        next(error);
+        next(error)
     }
-};
+}
 
 const getPostById = async (req, res, next) => {
     try {
-        const posts = await Post.findById(req.params.id);
+        const posts = await Post.findById(req.params.id)
         res.status(200).send({
             'status': 'ok',
             'posts': posts
         });
     } catch (error) {
-        next(error);
+        next(error)
     }
-};
+}
 
 const addNewPost = async (req, res, next) => {
     try {
