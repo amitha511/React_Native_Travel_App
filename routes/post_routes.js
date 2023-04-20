1d2bc80d0a2f7df484bbf26c495930b6bc15491b
Idankay <idanm399@cs.colman.ac.il>
2023-04-20 19:52:36 +0300

    node + db + swagger

--- routes/post_routes.js
+++ routes/post_routes.js
@@ -2,8 +2,85 @@ const express = require('express')
 const router = express.Router()
 
 const Post = require('../controllers/posts')
+
+/* API specifications written in YAML */
+
+/**
+* @swagger
+* tags:
+*   name: Post Api
+*   description: The Post API
+*/
+
+/**
+* @swagger
+* components:
+*   schemas:
+*     Post:
+*       type: object
+*       required:
+*         - message
+*         - sender
+*       properties:
+*         message:
+*           type: string
+*           description: The post text 
+*         sender:
+*           type: string
+*           description: The user who send the post id
+*       example:
+*         message: 'this is swagger test message'
+*         sender: '1'
+*/
+
 router.get('/', Post.getPosts)
+
+/**
+* @swagger
+* /post/{id}:
+*   get:
+*     summary: get all posts
+*     tags: [Post Api]
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: string
+*         required: true
+*         description: The post id
+*     responses:
+*       200:
+*         description: The posts list
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Post'
+*/
+
 router.get('/:id', Post.getPostById)
+
+/**
+* @swagger
+* /post:
+*   post:
+*     summary: add new post
+*     tags: [Post Api]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/Post'
+*     responses:
+*       200:
+*         description: The posts list
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Post'
+*/
+
 router.post('/', Post.addNewPost)
 
 module.exports = router;
+
