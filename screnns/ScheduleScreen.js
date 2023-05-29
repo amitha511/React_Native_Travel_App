22b349c0e499714365847ce98da44332b51ba6df
bennizri <bennizri22@gmail.com>
2023-05-29 18:24:39 +0300

    ScheduleScreen

--- screnns/ScheduleScreen.js
+++ screnns/ScheduleScreen.js
@@ -1,228 +1,89 @@
-import React, { useState } from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  Button,
-  StyleSheet,
-  ScrollView,
-} from "react-native";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, ScrollView, ActivityIndicator } from "react-native";
+import Timeline from "react-native-beautiful-timeline";
+import * as Animatable from "react-native-animatable";
+import moment from "moment";
 
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+function Schedule() {
+  const [receiveData, setReceiveData] = useState(false);
+  const [responseData, setResponseData] = useState(null);
 
-const Schedule = () => {
-  const hours = Array.from({ length: 24 }, (_, i) => i);
-
-  const [attractions, setAttractions] = useState([
-    { id: 1, name: "Default Attraction", day: "Sunday", start: 2, end: 4 },
-  ]);
-
-  const addAttraction = () => {
-    const newAttraction = {
-      id: Date.now(),
-      name: "New Attraction",
-      day: "Sunday",
-      start: 0,
-      end: 1,
-    };
-    setAttractions([...attractions, newAttraction]);
-  };
-
-  const updateAttraction = (id, field, value) => {
-    setAttractions(
-      attractions.map((attr) => {
-        if (attr.id === id) {
-          if (field === "start" || field === "end") {
-            const newStart = field === "start" ? value : parseInt(attr.start);
-            const newEnd = field === "end" ? value : parseInt(attr.end);
-
-            if (newStart < newEnd) {
-              // Check if the updated attraction overlaps with any existing attractions
-              const overlap = attractions.some(
-                (otherAttraction) =>
-                  otherAttraction.day === attr.day &&
-                  otherAttraction.id !== attr.id &&
-                  ((newStart >= otherAttraction.start &&
-                    newStart < otherAttraction.end) ||
-                    (newEnd > otherAttraction.start &&
-                      newEnd <= otherAttraction.end))
-              );
-
-              if (!overlap) {
-                return { ...attr, [field]: value };
-              }
-            }
-          } else {
-            return { ...attr, [field]: value };
-          }
-        }
-        return attr;
+  useEffect(() => {
+    axios
+      .get("http://192.168.1.70:4000/travel/get")
+      .then((response) => {
+        setResponseData(response.data);
+        setReceiveData(true);
       })
-    );
-  };
-  const deleteAttraction = (id) => {
-    setAttractions(attractions.filter((attr) => attr.id !== id));
-  };
+      .catch((error) => console.error(`Error${error}`));
+  }, []);
 
-  return (
+  let data = [];
+
+  if (responseData) {
+    data = responseData.map((item) => {
+      const dates = item.dates.map((date) =>
+        moment(date, "YYYY-MM-DD").toDate()
+      );
+
+      const attractionsData = Object.entries(item.attractions || {}).flatMap(
+        ([day, attractions]) => {
+          const dayNumber = Number(day.slice(3)); // Extract day number
+
+          const dailyAttractions = (attractions.dailyAttractions || []).map(
+            (attraction, index) => {
+              const currentDate = Math.floor(
+                dates[dayNumber - 1].getTime() / 1000
+              );
+              return {
+                title: attraction.name,
+                subtitle: attraction.rating,
+                date: currentDate,
+                vicinity: attraction.vicinity,
+                day: dayNumber, // Add day information to each attraction
+              };
+            }
+          );
+          return dailyAttractions;
+        }
+      );
+
+      return {
+        date: dates.map((date) => Math.floor(date.getTime() / 1000)),
+        data: attractionsData,
+      };
+    });
+  }
+
+  return receiveData ? (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Timeline data={data} />
+    </ScrollView>
+  ) : (
     <View style={styles.container}>
-      <ScrollView horizontal>
-        <View style={styles.times}>
-          <Text></Text>
-          {hours.map((hour) => (
-            <Text key={hour}>{`${hour}:00`}</Text>
-          ))}
-        </View>
-        <View style={styles.days}>
-          {days.map((day) => (
-            <View key={day} style={styles.dayColumn}>
-              <Text style={styles.day}>{day}</Text>
-              {hours.map((hour) => (
-                <View key={hour} style={[styles.hour, styles.hourRow]}>
-                  {attractions.map((attraction) => {
-                    if (
-                      attraction.day === day &&
-                      parseInt(attraction.start) === hour
-                    ) {
-                      return (
-                        <View key={attraction.id} style={styles.attraction}>
-                          <TextInput
-                            value={attraction.name}
-                            onChangeText={(text) =>
-                              updateAttraction(attraction.id, "name", text)
-                            }
-                            style={styles.nameInput}
-                          />
-                          <TextInput
-                            value={`${attraction.start}`}
-                            onChangeText={(text) =>
-                              updateAttraction(
-                                attraction.id,
-                                "start",
-                                parseInt(text)
-                              )
-                            }
-                            style={styles.hourInput}
-                            keyboardType="numeric"
-                          />
-                          <TextInput
-                            value={`${attraction.end}`}
-                            onChangeText={(text) =>
-                              updateAttraction(
-                                attraction.id,
-                                "end",
-                                parseInt(text)
-                              )
-                            }
-                            style={styles.hourInput}
-                            keyboardType="numeric"
-                          />
-                          <Button
-                            title="Delete"
-                            onPress={() => deleteAttraction(attraction.id)}
-                          />
-                        </View>
-                      );
-                    }
-                    return null;
-                  })}
-                </View>
-              ))}
-            </View>
-          ))}
-        </View>
-      </ScrollView>
-      <View style={styles.addButtonContainer}>
-        <Button title="Add Attraction" onPress={addAttraction} />
-      </View>
+      <Animatable.Text
+        animation="fadeIn"
+        duration={1000}
+        style={styles.loadingText}
+      >
+        Loading...
+      </Animatable.Text>
+      <ActivityIndicator />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     backgroundColor: "#fff",
-    flexDirection: "column",
-  },
-  times: {
-    marginRight: 10,
-    justifyContent: "space-between",
-    backgroundColor: "#f2f2f2",
-    paddingVertical: 5,
-    paddingHorizontal: 10,
-  },
-  days: {
-    flex: 1,
-    flexDirection: "row",
-  },
-  dayColumn: {
-    width: 200,
-    borderRightColor: "#ccc",
-    borderRightWidth: 1,
-    paddingTop: 10,
-    paddingBottom: 10,
     alignItems: "center",
-  },
-  day: {
-    fontWeight: "bold",
-    paddingBottom: 10,
-  },
-  hour: {
-    paddingBottom: 10,
-    borderBottomColor: "#ccc",
-    borderBottomWidth: 1,
-  },
-  hourLine: {
-    borderTopColor: "#ccc",
-    borderTopWidth: 1,
-    marginHorizontal: -10,
-  },
-  attraction: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 5,
-  },
-  nameInput: {
-    flex: 1,
-    borderColor: "#ccc",
-    borderWidth: 1,
-    paddingLeft: 5,
-    paddingRight: 5,
-    marginRight: 10,
-  },
-  hourInput: {
-    width: 50,
-    borderColor: "#ccc",
-    borderWidth: 1,
-    paddingLeft: 5,
-    paddingRight: 5,
-    marginLeft: 10,
-    marginRight: 10,
-  },
-  addButtonContainer: {
-    marginTop: 20,
     justifyContent: "center",
-    alignItems: "center",
   },
-  addButton: {
-    backgroundColor: "#4CAF50",
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-  },
-  addButtonText: {
-    color: "#fff",
-    fontWeight: "bold",
-    textTransform: "uppercase",
+  loadingText: {
+    fontSize: 20,
+    marginBottom: 10,
   },
 });
 
